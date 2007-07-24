b156b10d711a7d97ddae408f159d53de5170bcef
Benedict Eastaugh <benedict@eastaugh.net>
2007-07-24 17:18:02 +0000

    Refactored comments.php.
    git-svn-id: https://tarski.googlecode.com/svn/trunk@62 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- comments.php
+++ comments.php
@@ -1,180 +1,142 @@
-<?php global $trackbackLink;
+<?php // comments.php - Tarski comments file
 
-// Do not delete these lines
-if('comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-	die ('Please do not load this page directly. Thanks!');
-	if(!empty($post->post_password)) { // if there's a password
-		if($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {  // and it doesn't match the cookie
-?>
-	<p><?php _e('This post is password protected. Enter the password to view comments.','tarski'); ?><p>
-<?php
+global $trackbackLink;
+$oddcomment = 'alt'; // var for alternating comment background
+
+if('comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) {
+	die('Please do not load this page directly. Thanks!');
+}
+
+if(!empty($post->post_password)) { // if there's a password
+	if($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {  // and it doesn't match the cookie
+		echo '<p>' . __('This post is password protected. Enter the password to view comments.','tarski') . '</p>' . "\n";
 		return;
 	}
 }
 
-/* This variable is for alternating comment background */
-$oddcomment = 'alt';
-
-?>
-
-<?php if(have_comments()) { ?>
+if(have_comments() || ($post->comment_status == 'open')) { ?>
 <div id="comments">
-<?php if('open' == $post-> comment_status) { ?>
-
+	
 	<div class="meta">
 		<h2 class="title"><?php comments_number(__('No comments','tarski'), __('1 comment','tarski'), '%' . __(' comments','tarski')); ?></h2>
+		<?php if($post->comment_status == 'open') { ?>
 		<p class="comments-feed"><?php comments_rss_link(__('Comments feed for this article','tarski')); ?></p>
+		<?php } ?>
 		<?php if(pings_open()) { ?>
 		<div id="trackback-link">
 			<div class="secondary"><p><?php _e('Trackback link','tarski'); ?></p></div>
 			<div class="primary"><p><a href="<?php echo $trackbackLink; ?>"><?php echo $trackbackLink; ?></a></p></div>
 		</div>
 		<?php } ?>
-	</div> <!-- close comments header div -->
-
-<?php } else { // comments are closed ?>
-
-	<div id="meta">
-		<h2 class="title"><?php comments_number(__('No comments','tarski'), __('1 comment','tarski'), '%' . __(' comments','tarski')); ?></h2>
 	</div> <!-- /comments header -->
-
-<?php } ?>
-
-
-
-
-
-
-
-
-
-
-<?php foreach ($comments as $comment) : ?>
-	<?php if ($comment->comment_type == "trackback" || $comment->comment_type == "pingback" || ereg("<pingback />", $comment->comment_content) || ereg("<trackback />", $comment->comment_content)) { ?>
+	
+	<?php if(have_comments()) { ?>
+	
+		<?php foreach($comments as $comment) { // Run comments loop for track- and pingbacks ?>
 		
-	<div class="trackback" id="comment-<?php comment_ID() ?>">
-		<div class="secondary">
-			<p><a href="#comment-<?php comment_ID() ?>" title="<?php _e('Permalink to this comment','tarski'); ?>"><?php comment_date(); _e(' at ','tarski'); comment_time(); ?></a></p>
-		</div>
-		<div class="primary">
-			<p>
-			<?php preg_match("@^<strong>(.*?)</strong>@", $comment->comment_content, $matches); if($matches[1]) { ?>
-				<?php comment_type(__('Comment','tarski'), __('Trackback','tarski'), __('Pingback','tarski')); echo ' ' . __(' from','tarski'); ?> <strong><?php comment_author(); ?></strong><?php echo " - <a href=\""; comment_author_url(); echo "\">" . $matches[1] . "</a>"; ?>
-			<?php } else { ?>
-				<?php comment_type(__('Comment','tarski'), __('Trackback','tarski'), __('Pingback','tarski')); echo ' ' . __(' from','tarski'); ?> <strong><?php echo "<a href=\""; comment_author_url(); echo "\">"; comment_author(); echo "</a>"; ?></strong>
+			<?php if($comment->comment_type == "trackback" || $comment->comment_type == "pingback" || ereg("<pingback />", $comment->comment_content) || ereg("<trackback />", $comment->comment_content)) { ?>
+
+				<div class="trackback" id="comment-<?php comment_ID() ?>">
+					<div class="secondary">
+						<p><a href="#comment-<?php comment_ID() ?>" title="<?php _e('Permalink to this comment','tarski'); ?>"><?php comment_date(); _e(' at ','tarski'); comment_time(); ?></a></p>
+					</div>
+					<div class="primary">
+						<p>
+						<?php preg_match("@^<strong>(.*?)</strong>@", $comment->comment_content, $matches); if($matches[1]) { ?>
+							<?php comment_type(__('Comment','tarski'), __('Trackback','tarski'), __('Pingback','tarski')); echo ' ' . __(' from','tarski'); ?> <strong><?php comment_author(); ?></strong><?php echo " - <a href=\""; comment_author_url(); echo "\">" . $matches[1] . "</a>"; ?>
+						<?php } else { ?>
+							<?php comment_type(__('Comment','tarski'), __('Trackback','tarski'), __('Pingback','tarski')); echo ' ' . __(' from','tarski'); ?> <strong><?php echo "<a href=\""; comment_author_url(); echo "\">"; comment_author(); echo "</a>"; ?></strong>
+						<?php } ?>
+						<?php edit_comment_link(__('edit','tarski'), '(', ')'); ?></p>
+					</div>
+				</div> <!-- /trackback -->
+		
 			<?php } ?>
-			<?php edit_comment_link(__('edit','tarski'), '(', ')'); ?></p>
-		</div>
-	</div> <!-- /trackback -->
-	<?php } // end if trackback ?>
-<?php endforeach; /* end for each comment */ ?>
+			
+		<?php } foreach($comments as $comment) { // Run comments loop again for normal comments ?>
 
+			<?php if($comment->comment_type != "trackback" && $comment->comment_type != "pingback" && !ereg("<pingback />", $comment->comment_content) && !ereg("<trackback />", $comment->comment_content)) { $comment_count++; ?>
 
-<?php foreach ($comments as $comment) { $comment_count++; ?>
-	<?php if ($comment->comment_type != "trackback" && $comment->comment_type != "pingback" && !ereg("<pingback />", $comment->comment_content) && !ereg("<trackback />", $comment->comment_content)) { ?>
+				<div class="comment<?php
+				// Style differently if comment author is blog author
+				if ($comment->comment_author_email == get_the_author_email()) { echo ' author-comment'; }
+				if ($comment->comment_approved == '0') { echo ' moderated'; } ?>" id="comment-<?php comment_ID() ?>">
+					<?php if ($comment->comment_approved == '0') { ?>
+					<p class="primary-span"><strong><?php _e('Your comment is awaiting moderation.','tarski'); ?></strong></p>
+					<?php } ?>
+					<div class="secondary">
+						<p class="comment-permalink"><a href="#comment-<?php comment_ID(); ?>" title="<?php _e('Permalink to this comment','tarski'); ?>"><?php comment_date(); _e(' at ','tarski'); comment_time() ?></a></p>
+						<p class="comment-author"><strong><?php $comment_author_name = tarski_get_output('comment_author_link();'); $comment_author_name = str_replace('>http://', '>', $comment_author_name); $comment_author_name = str_replace('/</a>', '</a>', $comment_author_name); echo $comment_author_name; ?></strong></p>
+						<?php edit_comment_link(__('edit','tarski'), '<p class="comment-permalink">(', ')</p>'); ?> 
+					</div> <!-- /comment meta -->
+
+					<div class="primary content">
+						<?php
+						if (function_exists('gravatar')) {
+							if(get_comment_author_url()) {
+								echo "<a href=\"" . get_comment_author_url() . "\">";
+							}
+							echo "<img class=\"gravatar\" src=\"";
+							gravatar($comment->comment_author_email);
+							echo "\" alt=\"\" />";
+							if(get_comment_author_url()) {
+								echo "</a>";
+							}
+						}
+						?>
+						<?php comment_text() ?>
+					</div> <!-- /comment content -->
+				</div> <!-- /comment -->
 		
-	<div class="comment<?php
-	// Style differently if comment author is blog author
-	if ($comment->comment_author_email == get_the_author_email()) { echo ' author-comment'; }
-	if ($comment->comment_approved == '0') { echo ' moderated'; } ?>" id="comment-<?php comment_ID() ?>">
-		<?php if ($comment->comment_approved == '0') { ?>
-		<p class="primary-span"><strong><?php _e('Your comment is awaiting moderation.','tarski'); ?></strong></p>
-		<?php } ?>
-		<div class="secondary">
-			<p class="comment-permalink"><a href="#comment-<?php comment_ID(); ?>" title="<?php _e('Permalink to this comment','tarski'); ?>"><?php comment_date(); _e(' at ','tarski'); comment_time() ?></a></p>
-			<p class="comment-author"><strong><?php $comment_author_name = tarski_get_output('comment_author_link();'); $comment_author_name = str_replace('>http://', '>', $comment_author_name); $comment_author_name = str_replace('/</a>', '</a>', $comment_author_name); echo $comment_author_name; ?></strong></p>
-			<?php edit_comment_link(__('edit','tarski'), '<p class="comment-permalink">(', ')</p>'); ?> 
-		</div> <!-- /comment meta -->
-		
-		<div class="primary content">
-			<?php
-			if (function_exists('gravatar')) {
-				if(get_comment_author_url()) {
-					echo "<a href=\"" . get_comment_author_url() . "\">";
-				}
-				echo "<img class=\"gravatar\" src=\"";
-				gravatar($comment->comment_author_email);
-				echo "\" alt=\"\" />";
-				if(get_comment_author_url()) {
-					echo "</a>";
-				}
-			}
-			?>
-			<?php comment_text() ?>
-		</div> <!-- /comment content -->
-	</div> <!-- /comment -->
-	<?php } // end if not trackback ?>
-<?php } /* end for each comment */ ?>
-
-
-
-
-
-
+			<?php } // end comment type eval ?>
+		<?php } // end foreach loop ?>
+	<?php } // end if comments loop ?>
 </div>
-<?php } else { // this is displayed if there are no comments so far ?>
 
-<?php if('open' == $post-> comment_status) { ?>
-<div id="comments">
-	<div class="meta">
-		<h2 class="title"><?php comments_number(__('No comments','tarski'), __('1 comment','tarski'), '%' . __(' comments','tarski')); ?></h2>
-		<p class="comments-feed"><?php comments_rss_link(__('Comments feed for this article','tarski')); ?></p>
-		<?php if(pings_open()) { ?>
-		<div id="trackback-link">
-			<div class="secondary"><p><?php _e('Trackback link','tarski'); ?></p></div>
-			<div class="primary"><p><a href="<?php echo $trackbackLink; ?>"><?php echo $trackbackLink; ?></a></p></div>
-		</div>
-		<?php } ?>
-	</div> <!-- /comment content -->
-</div> <!-- /comment -->
-<?php } else { // comments are closed ?>
 
-<?php } } if('open' == $post-> comment_status) { ?>
+<?php } if($post->comment_status == 'open') { ?>
 
 <div id="respond">
-
-<?php // if registration is mandatory
-	if(get_option('comment_registration') && !$user_ID) { ?>
+	
+<?php if(get_option('comment_registration') && !$user_ID) {  // if registration is mandatory and user not logged in ?>
+	
 	<div class="content">
 		<p><em><?php _e('You must be ','tarski'); ?><a href="<?php echo get_option('siteurl'); echo '/wp-login.php?redirect_to='; the_permalink(); ?>"><?php _e('logged in','tarski'); ?></a><?php _e(' to post a comment.','tarski'); ?></em></p>
 	</div>
 </div>
-<?php } else { ?>
 
-		<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform"><fieldset>
+<?php } else { // if registration is not mandatory ?>
+	
+	<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform"><fieldset>
 
-<?php // if user is logged in
-	if($user_ID) { ?>
-
+	<?php if($user_ID) { // if user is logged in ?>
+		
 		<div id="info-input" class="secondary content">
 			<p class="userinfo"><?php _e('You are logged in as ','tarski'); ?><a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>.</p>
 			<p><a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="<?php _e('Log out of this account','tarski') ?>"><?php _e('Logout &raquo;','tarski'); ?></a></p>
 			<?php if(function_exists('show_subscription_checkbox')) { show_subscription_checkbox(); } ?>
 		</div> <!-- /info fields -->
 
-<?php // if user is not logged in - name, email and website fields
-	} else { ?>
+	<?php } else { // if user is not logged in - name, email and website fields ?>
+		
+		<div id="info-input" class="secondary content">
+			<label for="author"><?php _e('Name','tarski'); ?><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" /></label>
+			<label for="email"><?php _e('Email','tarski'); ?><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" /></label>
+			<label for="url"><?php _e('Website','tarski'); ?><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" /></label>
+			<?php if(function_exists('show_subscription_checkbox')) { show_subscription_checkbox(); } ?>
+		</div> <!-- /info fields -->
 
-			<div id="info-input" class="secondary content">
-				<label for="author"><?php _e('Name','tarski'); ?><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" /></label>
-				<label for="email"><?php _e('Email','tarski'); ?><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" /></label>
-				<label for="url"><?php _e('Website','tarski'); ?><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" /></label>
-				<?php if(function_exists('show_subscription_checkbox')) { show_subscription_checkbox(); } ?>
-			</div> <!-- /info fields -->
-
-
-<?php // actual comment form
-	} ?>
-			<div id="comment-input" class="primary">
-				<label for="comment"><?php _e('Your comment','tarski'); ?></label>
-				<textarea name="comment" id="comment" cols="60" rows="12" tabindex="4"></textarea>
-				<input name="submit" type="submit" id="submit" tabindex="5" value="<?php _e('Submit Comment','tarski'); ?>" />
-				<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
-				<?php th_commentform(); ?>
-			</div>  <!-- /comment input -->
-<?php do_action('comment_form', $post->ID); ?>
-		</fieldset></form>
-	</div> <!-- /comment form -->
-
-<?php } // If registration required and not logged in ?>
-<?php } // if you delete this the sky will fall on your head / O RLY / YA RLY ?>
+	<?php } // textarea etc. start here ?>
+	
+		<div id="comment-input" class="primary">
+			<label for="comment"><?php _e('Your comment','tarski'); ?></label>
+			<textarea name="comment" id="comment" cols="60" rows="12" tabindex="4"></textarea>
+			<input name="submit" type="submit" id="submit" tabindex="5" value="<?php _e('Submit Comment','tarski'); ?>" />
+			<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+			<?php th_commentform(); ?>
+		</div>  <!-- /comment input -->
+	<?php do_action('comment_form', $post->ID); ?>
+	</fieldset></form>
+</div> <!-- /comment form -->
+<?php } // end registration check ?>
+<?php } // end form conditional ?>
