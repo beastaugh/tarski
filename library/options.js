c3b032377c4525652b7a7bb033d44b146eec1ca3
Benedict Eastaugh <benedict@eastaugh.net>
2007-08-13 16:47:02 +0000

    Tweaked options page JS
    git-svn-id: https://tarski.googlecode.com/svn/trunk@75 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- library/options.js
+++ library/options.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
 	// Sidebar switching code - needs refactoring
 	$('input#option-ts').change(function() {
-		$('div#tarski-sidebar-section').toggle();
-		$('div#widgets-sidebar-section').toggle();
-		$('div#custom-sidebar-section').toggle();
+		$('div#tarski-sidebar-section').show();
+		$('div#widgets-sidebar-section').hide();
+		$('div#custom-sidebar-section').hide();
 	});
 	$('input#option-ws').change(function() {
-		$('div#tarski-sidebar-section').toggle();
-		$('div#widgets-sidebar-section').toggle();
-		$('div#custom-sidebar-section').toggle();
+		$('div#tarski-sidebar-section').hide();
+		$('div#widgets-sidebar-section').show();
+		$('div#custom-sidebar-section').hide();
 	});
 	$('input#option-fs').change(function() {
-		$('div#tarski-sidebar-section').toggle();
-		$('div#widgets-sidebar-section').toggle();
-		$('div#custom-sidebar-section').toggle();
+		$('div#tarski-sidebar-section').hide();
+		$('div#widgets-sidebar-section').hide();
+		$('div#custom-sidebar-section').show();
 	});
-	
-})
+});
