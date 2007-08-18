7ef20850e21345c7fe706e5dcdee81de87088a52
Benedict Eastaugh <benedict@eastaugh.net>
2007-08-18 17:24:06 +0000

    Removed  global
    git-svn-id: https://tarski.googlecode.com/svn/trunk@91 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- searchform.php
+++ searchform.php
@@ -1,4 +1,3 @@
-<?php global $s; ?>
 <div class="searchbox">
 	<form method="get" id="searchform" action="<?php echo get_bloginfo('url') . '/'; ?>"><fieldset>
 		<input type="text" value="<?php if(the_search_query()) { echo the_search_query(); } else { _e('Search this site','tarski'); } ?>" name="s" id="s" tabindex="21" />
