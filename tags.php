5fdf2ffa96e52543f89426d61b3a6e7d34ec981d
Benedict Eastaugh <benedict@eastaugh.net>
2007-08-24 23:18:59 +0000

    New tags page
    git-svn-id: https://tarski.googlecode.com/svn/trunk@113 8ec1c81d-c82d-0410-ab24-9d07384f4a97

---
+++ tags.php
@@ -0,0 +1,27 @@
+<?php /*
+Template Name: Tags
+*/ ?>
+<?php get_header(); ?>
+<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
+	
+	<div class="primary entry">
+		<div class="meta">
+			<h1 class="title"><?php the_title(); ?></h1>
+			<?php edit_post_link(__('edit page','tarski'), '<p class="metadata">(', ')</p>'); ?>
+		</div> <!-- /meta -->
+		
+		<div class="content">
+			<?php the_content(); ?>
+			
+			<h3><?php _e('Tags','tarski'); ?></h3>
+			
+			<?php wp_tag_cloud(); ?>
+		</div> <!-- /content -->
+
+		<?php link_pages_without_spaces(); ?>
+		<?php th_postend(); ?>
+	</div> <!-- /primary -->
+
+<?php } } ?>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
