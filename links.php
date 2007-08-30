b14634464b42ce20bcd437a760f43b9b0cc61ddd
Benedict Eastaugh <benedict@eastaugh.net>
2007-08-30 13:47:50 +0000

    Modernised links template
    git-svn-id: https://tarski.googlecode.com/svn/trunk@131 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- links.php
+++ links.php
@@ -1,18 +1,28 @@
-<?php
-/*
+<?php /*
 Template Name: Links
-*/
-?>
-<?php @include(TEMPLATEPATH . '/constants.php'); ?>
+*/ ?>
 <?php get_header(); ?>
-<div class="primary archive">
-	<div class="meta">
-		<h1><?php _e('Links', 'tarski'); ?></h1>
-	</div>
-	<div class="bookmarks">
-	<?php wp_list_bookmarks('category_before=&category_after=&title_before=<h3>&title_after=</h3>&show_images=0&show_description=0'); ?>
-	<?php echo $pageEndInclude; ?>
-	</div>
-</div>
+<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
+	
+	<div class="primary entry">
+		<div class="meta">
+			<h1 class="title"><?php the_title(); ?></h1>
+			<?php edit_post_link(__('edit page','tarski'), '<p class="metadata">(', ')</p>'); ?>
+		</div> <!-- /meta -->
+		
+		<?php if(get_the_content() != "") { ?>
+			<div class="content">
+				<?php the_content(); ?>
+			</div> <!-- /content -->
+		<?php } ?>
+		<div class="bookmarks">
+			<?php wp_list_bookmarks('category_before=&category_after=&title_before=<h3>&title_after=</h3>&show_images=0&show_description=0'); ?>
+		</div> <!-- /bookmarks -->
+
+		<?php link_pages_without_spaces(); ?>
+		<?php th_postend(); ?>
+	</div> <!-- /primary -->
+
+<?php } } ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
