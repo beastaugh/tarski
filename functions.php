ddaf32f17a2a22bc26dcdf87404f30f010bdc416
Benedict Eastaugh <benedict@eastaugh.net>
2007-10-14 22:30:05 +0000

    Tweaked functions.php style
    git-svn-id: https://tarski.googlecode.com/svn/trunk@290 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- functions.php
+++ functions.php
@@ -1,34 +1,34 @@
 <?php
 
 // Path constants
-define("TARSKILIB", TEMPLATEPATH . "/library");
-define("TARSKICLASSES", TARSKILIB . "/classes");
-define("TARSKIHELPERS", TARSKILIB . "/helpers");
-define("TARSKIDISPLAY", TARSKILIB . "/display");
-define("TARSKICACHE", TARSKILIB . "/cache");
-define("TARSKIVERSIONFILE", "http://tarskitheme.com/version.atom");		
+define('TARSKILIB', TEMPLATEPATH . '/library');
+define('TARSKICLASSES', TARSKILIB . '/classes');
+define('TARSKIHELPERS', TARSKILIB . '/helpers');
+define('TARSKIDISPLAY', TARSKILIB . '/display');
+define('TARSKICACHE', TARSKILIB . '/cache');
+define('TARSKIVERSIONFILE', 'http://tarskitheme.com/version.atom');		
 
 // Classes
-require_once(TARSKICLASSES."/options.php");
-require_once(TARSKICLASSES."/version.php");
+require_once(TARSKICLASSES . '/options.php');
+require_once(TARSKICLASSES . '/version.php');
 
 // Helpers
-include_once(TARSKIHELPERS."/hooks.php");
-include_once(TARSKIHELPERS."/admin_helper.php");
-include_once(TARSKIHELPERS."/template_helper.php");
-include_once(TARSKIHELPERS."/content_helper.php");
-include_once(TARSKIHELPERS."/author_helper.php");
-include_once(TARSKIHELPERS."/constants_helper.php");
-include_once(TARSKIHELPERS."/cache_helper.php");
-include_once(TARSKIHELPERS."/deprecated.php");
+include_once(TARSKIHELPERS . '/hooks.php');
+include_once(TARSKIHELPERS . '/admin_helper.php');
+include_once(TARSKIHELPERS . '/template_helper.php');
+include_once(TARSKIHELPERS . '/content_helper.php');
+include_once(TARSKIHELPERS . '/author_helper.php');
+include_once(TARSKIHELPERS . '/constants_helper.php');
+include_once(TARSKIHELPERS . '/cache_helper.php');
+include_once(TARSKIHELPERS . '/deprecated.php');
 
 // Options
-load_theme_textdomain("tarski");
+load_theme_textdomain('tarski');
 global $tarski_options;
 flush_tarski_options();
-@include(TEMPLATEPATH . "/constants.php");
+@include(TEMPLATEPATH . '/constants.php');
 
 // Launch
-require_once(TARSKILIB . "/launcher.php");
+require_once(TARSKILIB . '/launcher.php');
 
 ?>
