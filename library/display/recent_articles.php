4144a6d1f88646a19b608e5d188dcc0b879828e2
Benedict Eastaugh <benedict@eastaugh.net>
2007-10-18 15:43:29 +0000

    Added missing closing bracket
    git-svn-id: https://tarski.googlecode.com/svn/trunk@297 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- library/display/recent_articles.php
+++ library/display/recent_articles.php
@@ -5,7 +5,7 @@
 			<li>
 				<h4 class="recent-title"><a title="<?php _e('View this post', 'tarski'); ?>" href="<?php the_permalink(); ?>"><?php the_title() ?></a></h4>
 				<p class="recent-metadata"><?php echo tarski_date(); if(!get_tarski_option('hide_categories')) { _e(' in ', 'tarski'); the_category(', '); } ?></p>
-				<p class="recent-excerpt content"><?php echo strip_tags(tarski_excerpt(35, '', 'the_content', false, '', false, 1, true); ?></p>
+				<p class="recent-excerpt content"><?php echo strip_tags(tarski_excerpt(35, '', 'the_content', false, '', false, 1, true)); ?></p>
 			</li>
 		<?php } ?>
 	</ul>
