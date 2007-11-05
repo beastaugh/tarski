d84671fc0ad3e7affc506ef289aeef39519fa5c0
Benedict Eastaugh <benedict@eastaugh.net>
2007-11-05 18:47:12 +0000

    Fixed update notification showing when version is current
    git-svn-id: https://tarski.googlecode.com/svn/trunk@330 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- library/display/admin/version_options.php
+++ library/display/admin/version_options.php
@@ -1,8 +1,9 @@
-<?php if(get_tarski_option("update_notification")) { ?>
-	<?php if($status == "not_current") ?>
+<?php if(get_tarski_option('update_notification')) { ?>
+	<?php if($status == 'not_current') { ?>
 		<div id="tarski_update_notification" class="updated">
 			<p>
 				<?php printf( __('A new version of the Tarski theme, version %1$s, %2$s. Your installed version is %3$s.','tarski'), '<strong>'. $latest. '</strong>', '<a href="'. $latest_link. '">'. __('is now available','tarski'). '</a>', '<strong>'. $current. '</strong>' ); ?>
 			</p>
 		</div>
+	<?php } ?>
 <?php } ?>
