d1a5e207560cdcd09985491bf728ee50526226e7
Benedict Eastaugh <benedict@eastaugh.net>
2007-12-19 10:13:24 +0000

    Fixed feed MIME type for 2.3
    git-svn-id: https://tarski.googlecode.com/svn/trunk@430 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- library/helpers/template_helper.php
+++ library/helpers/template_helper.php
@@ -436,7 +436,7 @@ function tarski_feeds() {
 	if($title && $link)
 		$feeds[$source] = generate_feed_link($title, $link, $type);
 	
-	$feeds['site'] = generate_feed_link( sprintf(__('%s feed','tarski'), get_bloginfo('name')), get_feed_link(), $type );
+	$feeds['site'] = generate_feed_link( sprintf(__('%s feed','tarski'), get_bloginfo('name')), get_feed_link(), feed_link_type($type) );
 	$feeds = apply_filters('tarski_feeds', $feeds);
 	
 	// Filters should return an array
