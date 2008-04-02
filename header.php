f850a162544fcf901cb324589095595b4e366722
Benedict Eastaugh <benedict@eastaugh.net>
2008-04-02 10:12:50 +0000

    Add XFN profile attribute to head element
    git-svn-id: https://tarski.googlecode.com/svn/trunk@525 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes('xhtml'); ?>>
 
-<head><title><?php echo tarski_doctitle(); ?></title>
+<head profile="http://gmpg.org/xfn/11"><title><?php echo tarski_doctitle(); ?></title>
 
 	<meta http-equiv="Content-Type" content="<?php echo get_bloginfo('html_type') .'; charset=' . get_bloginfo('charset'); ?>" />	
 	<?php wp_head(); ?>
