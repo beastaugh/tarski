95abea638805f2bdf5ecfba01da6066b5bf7fb23
Benedict Eastaugh <benedict@eastaugh.net>
2008-04-06 16:28:12 +0000

    Add links around avatars
    git-svn-id: https://tarski.googlecode.com/svn/trunk@535 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- library/helpers/content_helper.php
+++ library/helpers/content_helper.php
@@ -248,10 +248,15 @@ function tidy_openid_names($comment_author) {
  * @return mixed
  */
 function tidy_avatars($avatar, $id_or_email, $size, $default) {
+	$url = get_comment_author_url();
 	$author_alt = sprintf( __('%s&#8217;s avatar'), get_comment_author() );
 	$avatar = preg_replace("/height='[\d]+' width='[\d]+'/", '', $avatar);
 	$avatar = preg_replace("/'/", '"', $avatar);
-	$avatar = preg_replace('/alt=""/', "alt=\"$author_alt\"", $avatar);	
+	$avatar = preg_replace('/alt=""/', "alt=\"$author_alt\"", $avatar);
+	
+	if( !empty($url) && 'http://' != $url )
+		$avatar = "<a href=\"$url\">$avatar</a>";
+		
 	return $avatar;
 }
 
