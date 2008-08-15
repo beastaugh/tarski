3901ab57aa145b564a58b108b54eeee6ddee96af
Benedict Eastaugh <benedict@eastaugh.net>
2008-08-15 16:21:35 +0000

    Improve ease of localisation for comments template
    git-svn-id: https://tarski.googlecode.com/svn/trunk@787 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- comments.php
+++ comments.php
@@ -1,7 +1,7 @@
 <?php
 
 if('comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) {
-	die('Please do not load this page directly. Thanks!');
+	die(__('This page should not be loaded directly.', 'tarski'));
 }
 
 if(!empty($post->post_password)) { // if there's a password
@@ -35,7 +35,7 @@ if($comments || comments_open()) { ?>
 
 				<div class="trackback clearfix" id="comment-<?php comment_ID() ?>">
 					<div class="secondary">
-						<p><a href="#comment-<?php comment_ID() ?>" title="<?php _e('Permalink to this comment','tarski'); ?>"><?php comment_date(); _e(' at ','tarski'); comment_time(); ?></a></p>
+						<p><a href="#comment-<?php comment_ID() ?>" title="<?php _e('Permalink to this comment','tarski'); ?>"><?php printf(__('%1$s at %2$s', 'tarski'), get_comment_date(), get_comment_time()); ?></a></p>
 					</div>
 					<div class="primary">
 						<p>
@@ -85,7 +85,10 @@ if($comments || comments_open()) { ?>
 <?php if(get_option('comment_registration') && !$user_ID) {  // if registration is mandatory and user not logged in ?>
 	
 	<div class="content">
-		<p><em><?php _e('You must be ','tarski'); ?><a href="<?php echo site_url('wp-login.php?redirect_to=' . get_permalink()); ?>"><?php _e('logged in','tarski'); ?></a><?php _e(' to post a comment.','tarski'); ?></em></p>
+		<p><em><?php printf(
+			__('You must be %s to post a comment.', 'tarski'),
+			'<a href="' . site_url('wp-login.php?redirect_to=' . get_permalink()) . '">' . __('logged in', 'tarski') . '</a>'
+		); ?></em></p>
 	</div>
 </div>
 
@@ -96,7 +99,11 @@ if($comments || comments_open()) { ?>
 	<?php if($user_ID) { // if user is logged in ?>
 		
 		<div id="info-input" class="secondary content">
-			<p class="userinfo"><?php _e('You are logged in as ','tarski'); ?><a href="<?php echo admin_url('profile.php'); ?>"><?php echo $user_identity; ?></a>.</p>
+			<p class="userinfo"><?php printf(
+				__('You are logged in as %s.', 'tarski'),
+				'<a href="' . admin_url('profile.php') . '">' . $user_identity . '</a>'
+			); ?></p>
+			
 			<p><a href="<?php echo site_url('wp-login.php?action=logout'); ?>" title="<?php _e('Log out of this account','tarski') ?>"><?php _e('Logout &raquo;','tarski'); ?></a></p>
 		</div> <!-- /info fields -->
 
