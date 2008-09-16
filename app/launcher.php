a32f9d6cffcefa8bbfe70b49d1d70e31ddecf3e1
Benedict Eastaugh <benedict@eastaugh.net>
2008-09-16 09:54:19 +0000

    Add options page style and scripts via updated hooks
    git-svn-id: https://tarski.googlecode.com/svn/trunk@792 8ec1c81d-c82d-0410-ab24-9d07384f4a97

--- app/launcher.php
+++ app/launcher.php
@@ -75,8 +75,10 @@ if (is_admin()) {
 
 	// Tarski Options page
 	add_action('admin_print_styles', 'tarski_admin_style');
-	add_action('admin_print_scripts-design_page_tarski-options', 'tarski_inject_scripts');
-	add_action('admin_print_styles-design_page_tarski-options', 'tarski_inject_styles');
+	add_action('admin_print_scripts-themes_page_tarski-options', 'tarski_inject_scripts');
+	add_action('admin_print_styles-themes_page_tarski-options', 'tarski_inject_styles');
+	add_action('admin_print_scripts-design_page_tarski-options', 'tarski_inject_scripts'); // Pre-2.7
+	add_action('admin_print_styles-design_page_tarski-options', 'tarski_inject_styles'); // Pre-2.7
 	add_action('admin_menu', 'tarski_addmenu');
 
 	// Options
