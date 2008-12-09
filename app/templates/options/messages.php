5b1854c72cbf0b06392b1946e39c7c9df2b8a66e
Benedict Eastaugh <benedict@eastaugh.net>
2008-12-09 14:05:15 +0000

    Add a bottom margin to messages on the options page.

--- app/templates/options/messages.php
+++ app/templates/options/messages.php
@@ -1,10 +1,10 @@
 <?php if (!get_tarski_option('deleted')) { if (isset($_GET['restored'])) { ?>
-	<div id="updated" class="updated fade below-h2"><p><?php printf(
+	<div class="updated fade below-h2"><p><?php printf(
 		__('Tarski options have been restored. %s', 'tarski'),
 		'<a href="' . user_trailingslashit(get_bloginfo('url')) . '">' . __('View site &rsaquo;','tarski') . '</a>'
 	); ?></p></div>
 <?php } elseif (isset($_GET['updated'])) { ?>
-	<div id="updated" class="updated fade below-h2"><p><?php printf(
+	<div class="updated fade below-h2"><p><?php printf(
 		__('Tarski options have been updated. %s', 'tarski'),
 		'<a href="' . user_trailingslashit(get_bloginfo('url')) . '">' . __('View site &rsaquo;','tarski') . '</a>'
 	); ?></p></div>
