430546d802a59b349c3cbf2e0620956075016c1b
Benedict Eastaugh <benedict@eastaugh.net>
2009-08-07 01:00:57 +0100

    Change DOCTYPE to HTML 5. Fixes gh-6.

--- header.php
+++ header.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes('xhtml'); ?>>
-
-<head profile="http://gmpg.org/xfn/11"><title><?php echo tarski_doctitle(); ?></title>
-
-	<meta http-equiv="Content-Type" content="<?php echo get_bloginfo('html_type') .'; charset=' . get_bloginfo('charset'); ?>" />	
+<!DOCTYPE html>
+<html <?php language_attributes('html'); ?>><head>
+	<meta http-equiv="Content-Type" content="<?php echo get_bloginfo('html_type') .'; charset=' . get_bloginfo('charset'); ?>" />
+	<title><?php echo tarski_doctitle(); ?></title>
+	
 	<?php wp_head(); ?>
 
-</head>
-
-<body id="<?php tarski_bodyid(); ?>" class="<?php tarski_bodyclass(); ?>"><div id="wrapper">
+</head><body id="<?php tarski_bodyid(); ?>" class="<?php tarski_bodyclass(); ?>"><div id="wrapper">
 
 <div id="header">
 
