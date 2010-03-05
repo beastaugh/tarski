8b76bc17d56e92cfa75b6bd04b20ff97fdc1761f
Benedict Eastaugh <benedict@eastaugh.net>
2010-03-05 14:44:40 +0000

    Standardise JavaScript on 4 space indentation.

--- app/js/header_select.js
+++ app/js/header_select.js
@@ -1,53 +1,53 @@
-var Radios = function(selector) {
-  this._elements = [];
-  var radios     = this;
-  jQuery(selector).each(function(i) {
-    radios.add(new Radio(radios, this));
-  });
+Radios = function(selector) {
+    this._elements = [];
+    var radios     = this;
+    jQuery(selector).each(function(i) {
+        radios.add(new Radio(radios, this));
+    });
 };
 
 Radios.prototype.add = function(item) {
-  if (item._input && item._label) this._elements.push(item);
+    if (item._input && item._label) this._elements.push(item);
 };
 
 Radios.prototype.check = function(radio) {
-  var i = this._elements.length;
-  while (i--) {
-    this._elements[i]._input.checked = false;
-    jQuery(this._elements[i]._label).removeClass('checked');
-  }
-  radio._input.checked = true;
-  jQuery(radio._label).addClass('checked');
+    var i = this._elements.length;
+    while (i--) {
+        this._elements[i]._input.checked = false;
+        jQuery(this._elements[i]._label).removeClass('checked');
+    }
+    radio._input.checked = true;
+    jQuery(radio._label).addClass('checked');
 };
 
 var Radio = function(group, input) {
-  this.group  = group;
-  this._input = input;
-  this._label = document.getElementById('for_' + this._input.id);
-  this._setup(this._group, this._input);
+    this.group  = group;
+    this._input = input;
+    this._label = document.getElementById('for_' + this._input.id);
+    this._setup(this._group, this._input);
 };
 
 Radio.prototype._setup = function(group, input) {
-  jQuery(this._input).wrap('<span style="position:relative;"></span>');
-  jQuery(this._input).css({position: 'absolute', left: '-9999em'});
-  
-  if (this._input.checked) jQuery(this._label).addClass('checked');
-  
-  var radio = this;
-  
-  jQuery([this._input, this._label]).click(function(event) {
-    radio.group.check(radio);
-  });
-  
-  jQuery(this._label).mouseover(function(event) {
-    jQuery(this).addClass('hovered');
-  });
-  
-  jQuery(this._label).mouseout(function(event) {
-    jQuery(this).removeClass('hovered');
-  });
+    jQuery(this._input).wrap('<span style="position:relative;"></span>');
+    jQuery(this._input).css({position: 'absolute', left: '-9999em'});
+    
+    if (this._input.checked) jQuery(this._label).addClass('checked');
+    
+    var radio = this;
+    
+    jQuery([this._input, this._label]).click(function(event) {
+        radio.group.check(radio);
+    });
+    
+    jQuery(this._label).mouseover(function(event) {
+        jQuery(this).addClass('hovered');
+    });
+    
+    jQuery(this._label).mouseout(function(event) {
+        jQuery(this).removeClass('hovered');
+    });
 }
 
 jQuery(document).ready(function() {
-  new Radios('#tarski-headers input[type=radio]');
+    new Radios('#tarski-headers input[type=radio]');
 });
--- app/js/page_select.js
+++ app/js/page_select.js
@@ -49,96 +49,96 @@ var UniqueNumList = function(memo, item) {
 };
 
 function CollapsibleList(container, collapsed_page_set) {
-	var id_format = /^page-list-(\d+)$/;
-	
-	this.container = jQuery(container);
-	this.root_id = parseInt(this.container.attr('id').replace(id_format, '$1'));
-	this.main = jQuery(container.children('p')[0]);
-	this.list = jQuery(container.children('ol')[0]);
-	
-	this.toggle = function() {
-		if ( this.container.hasClass('collapsed') ) {
-			this.expand();
-		} else {
-			this.collapse();
-		}
-	};
-	
-	this.collapse = function() {
-		this.list.slideUp(150);
-		this.toggler.addClass('collapsed-toggle');
-		this.container.addClass('collapsed');
-		collapsed_page_set.addPage(this.root_id);
-	};
-	
-	this.expand = function() {
-		this.list.slideDown(150);
+    var id_format = /^page-list-(\d+)$/;
+    
+    this.container = jQuery(container);
+    this.root_id = parseInt(this.container.attr('id').replace(id_format, '$1'));
+    this.main = jQuery(container.children('p')[0]);
+    this.list = jQuery(container.children('ol')[0]);
+    
+    this.toggle = function() {
+        if ( this.container.hasClass('collapsed') ) {
+            this.expand();
+        } else {
+            this.collapse();
+        }
+    };
+    
+    this.collapse = function() {
+        this.list.slideUp(150);
+        this.toggler.addClass('collapsed-toggle');
+        this.container.addClass('collapsed');
+        collapsed_page_set.addPage(this.root_id);
+    };
+    
+    this.expand = function() {
+        this.list.slideDown(150);
     this.toggler.removeClass('collapsed-toggle');
-		this.container.removeClass('collapsed');
-		collapsed_page_set.removePage(this.root_id);
-	};
-	
-	this.addToggle = function(toggler) {
-		var thisp = this;
-		this.toggler = jQuery(toggler);
-		
-		if (this.container.hasClass('collapsed')) this.toggler.addClass('collapsed-toggle');
-		
-		this.main.prepend(this.toggler);
-		this.toggler.bind('click', function(ev) {
-			thisp.toggle();
-		});
-	};
-	
+        this.container.removeClass('collapsed');
+        collapsed_page_set.removePage(this.root_id);
+    };
+    
+    this.addToggle = function(toggler) {
+        var thisp = this;
+        this.toggler = jQuery(toggler);
+        
+        if (this.container.hasClass('collapsed')) this.toggler.addClass('collapsed-toggle');
+        
+        this.main.prepend(this.toggler);
+        this.toggler.bind('click', function(ev) {
+            thisp.toggle();
+        });
+    };
+    
 };
 
 function CollapsedPageSet(selector) {
-	this.pages = [];
-	
-	this.retrieve = function() {
-		this.pages = jQuery(selector).val().split(',').reduce(UniqueNumList, []);
-	};
-	
-	this.save = function() {
-		this.pages = this.pages.reduce(UniqueNumList, []);
-		jQuery(selector).val(this.pages.join(','));
-	};
-	
-	this.addPage = function(page_id) {
-		this.retrieve();
-		
-		if (this.pages instanceof Array) {
-			this.pages.push(page_id);
-		} else {
-			this.pages = [page_id];
-		}
-		
-		this.save();
-	};
-	
-	this.removePage = function(page_id) {
-		this.retrieve();
-		
-		if (this.pages instanceof Array) {
-			this.pages = jQuery.grep(this.pages, function(n) {
-				return n != page_id;
-			});
-		} else {
-			this.pages = [];
-		}
-		
-		this.save();
-	};
-	
+    this.pages = [];
+    
+    this.retrieve = function() {
+        this.pages = jQuery(selector).val().split(',').reduce(UniqueNumList, []);
+    };
+    
+    this.save = function() {
+        this.pages = this.pages.reduce(UniqueNumList, []);
+        jQuery(selector).val(this.pages.join(','));
+    };
+    
+    this.addPage = function(page_id) {
+        this.retrieve();
+        
+        if (this.pages instanceof Array) {
+            this.pages.push(page_id);
+        } else {
+            this.pages = [page_id];
+        }
+        
+        this.save();
+    };
+    
+    this.removePage = function(page_id) {
+        this.retrieve();
+        
+        if (this.pages instanceof Array) {
+            this.pages = jQuery.grep(this.pages, function(n) {
+                return n != page_id;
+            });
+        } else {
+            this.pages = [];
+        }
+        
+        this.save();
+    };
+    
 };
 
 jQuery(document).ready(function() {
-	jQuery('#navbar-select').addClass('js');
-	
-	var collapsed_page_set = new CollapsedPageSet('#opt-collapsed-pages');
-	
-	jQuery('#navbar-select ol').each(function(i) {
-		list = new CollapsibleList(jQuery(this).parent(), collapsed_page_set);
-		list.addToggle('<span class="toggle">Toggle</span>');
-	});
+    jQuery('#navbar-select').addClass('js');
+    
+    var collapsed_page_set = new CollapsedPageSet('#opt-collapsed-pages');
+    
+    jQuery('#navbar-select ol').each(function(i) {
+        list = new CollapsibleList(jQuery(this).parent(), collapsed_page_set);
+        list.addToggle('<span class="toggle">Toggle</span>');
+    });
 });
