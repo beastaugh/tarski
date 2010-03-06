05e5e7f882445fd5318ebce31a6b20705f00e73c
Benedict Eastaugh <benedict@eastaugh.net>
2010-03-06 23:27:26 +0000

    Tidy up authors_helper and add @since notices.

--- library/helpers/author_helper.php
+++ library/helpers/author_helper.php
@@ -17,52 +17,58 @@
  */
 function tarski_author_posts_link($metadata) {
     global $authordata;
-
+    
     if (get_tarski_option('show_authors')) {
         $link = sprintf('<a href="%1$s" title="%2$s" class="url fn">%3$s</a>',
             get_author_posts_url($authordata->ID, $authordata->user_nicename),
             sprintf(__('Articles by %s','tarski'), esc_attr(get_the_author())),
             get_the_author());
-
+        
         $metadata .= __(' by ','tarski') .
             "<span class=\"vcard author\">$link</span>";
     }
-
+    
     return $metadata;
 }
 
 /**
- * the_archive_author() - Returns the author object associated with an author archive page.
- * 
+ * Returns the author object associated with an author archive page.
+ *
+ * @since 1.7
+ *
  * @global object $wp_query
  * @return object $current_author
  */
 function the_archive_author() {
-	global $wp_query;
-	$current_author = $wp_query->get_queried_object();
-	return $current_author;
+    global $wp_query;
+    $current_author = $wp_query->get_queried_object();
+    return $current_author;
 }
 
 /**
- * the_archive_author_displayname() - Returns the display name of the author associated with a given archive page.
+ * Returns the display name of the author associated with a given archive page.
  * 
+ * @since 1.7
+ *
  * @return string
  */
 function the_archive_author_displayname() {
-	$current_author = the_archive_author();
-	return $current_author->display_name;
+    $current_author = the_archive_author();
+    return $current_author->display_name;
 }
 
 /**
- * the_archive_author_description() - Returns the author description of the author associated with a given archive page.
- * 
+ * Returns the author description of the author associated with a given archive
+ * page.
+ *
+ * @since 1.7
+ *
  * @return string
  */
 function the_archive_author_description() {
-	$current_author = the_archive_author();
-	return isset($current_author->user_description) ?
-		$current_author->user_description :
-		'';
+    $current_author = the_archive_author();
+    return isset($current_author->user_description) ?
+        $current_author->user_description : '';
 }
 
 ?>
