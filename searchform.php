0174e9e7d883bafa17842f424d61c50824fb8dd3
Benedict Eastaugh <benedict@eastaugh.net>
2010-04-12 22:54:17 +0100

    Use search text field type in the search form.

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <?php $labelText = __('Search this site', 'tarski'); ?>
 <div class="searchbox">
-	<form method="get" id="searchform" action="<?php bloginfo('url'); ?>"><fieldset>
-		<label for="s" id="searchlabel"><?php echo $labelText ?></label>
-		<input type="text" placeholder="<?php echo $labelText ?>" value="<?php the_search_query(); ?>" name="s" id="s">
-		<input type="submit" id="searchsubmit" value="<?php _e('Search','tarski'); ?>">
-	</fieldset></form>
+    <form method="get" id="searchform" action="<?php bloginfo('url'); ?>"><fieldset>
+        <label for="s" id="searchlabel"><?php echo $labelText ?></label>
+        <input type="search" results="5" placeholder="<?php echo $labelText ?>" value="<?php the_search_query(); ?>" name="s" id="s">
+        <input type="submit" id="searchsubmit" value="<?php _e('Search','tarski'); ?>">
+    </fieldset></form>
 </div>
