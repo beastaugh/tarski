aeb59f074f970b7c4381a98bd2405d4212980437
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2010-09-24 08:00:29 -0700

    Use is_multisite in footer instead of detectWPMU.
    Also replaces references to the $current_site global with get_current_site and get_current_site_name.
    
    Signed-off-by: Benedict Eastaugh <benedict@eastaugh.net>

--- app/templates/credits.php
+++ app/templates/credits.php
@@ -1,6 +1,10 @@
 <div class="primary content">
-	<p><?php _e('Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://tarskitheme.com/">Tarski</a>', 'tarski');
-	if(detectWPMU()) {
-		echo ' | '. sprintf( __('Hosted by %s','tarski'), '<a href="http://'. $current_site->domain. $current_site->path. '">'. $current_site->site_name. '</a>' );
-	} ?></p>
+    <p><?php _e('Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://tarskitheme.com/">Tarski</a>', 'tarski');
+    if (is_multisite()) {
+        printf(__(' | Hosted by %s', 'tarski'),
+            '<a href="http://' .
+            get_current_site()->domain .
+            get_current_site()->path .
+            '">' . get_current_site_name() . '</a>');
+    } ?></p>
 </div>
