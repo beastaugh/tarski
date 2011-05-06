c1e126d25c1ac07d70218f1401db8ec3eac823f9
Benedict Eastaugh <benedict@eastaugh.net>
2011-05-06 15:27:00 +0100

    Remove syntactic incompatibility with PHP 4.

--- app/templates/credits.php
+++ app/templates/credits.php
@@ -1,10 +1,12 @@
 <div class="primary content">
     <p><?php _e('Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://tarskitheme.com/">Tarski</a>', 'tarski');
     if (is_multisite()) {
+        $current_site = get_current_site();
+        
         printf(__(' | Hosted by %s', 'tarski'),
             '<a href="http://' .
-            get_current_site()->domain .
-            get_current_site()->path .
-            '">' . get_current_site_name() . '</a>');
+            $current_site->domain .
+            $current_site->path .
+            '">' . get_current_site_name($current_site) . '</a>');
     } ?></p>
 </div>
