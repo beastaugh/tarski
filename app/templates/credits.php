faae642f2df10a1868473fee5960ff56e91239df
Benedict Eastaugh <benedict@eastaugh.net>
2011-11-01 15:44:39 +0000

    Pass current site to get_current_site function.
    Fixes a PHP warning.

--- app/templates/credits.php
+++ app/templates/credits.php
@@ -1,12 +1,12 @@
 <div class="primary content">
     <p><?php _e('Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://tarskitheme.com/">Tarski</a>', 'tarski');
     if (is_multisite()) {
-        $current_site = get_current_site_name();
+        $site = get_current_site_name(get_current_site());
         
         printf(__(' | Hosted by %s', 'tarski'),
             '<a href="http://' .
-            $current_site->domain .
-            $current_site->path .
-            '">' . $current_site->site_name . '</a>');
+            $site->domain .
+            $site->path .
+            '">' . $site->site_name . '</a>');
     } ?></p>
 </div>
