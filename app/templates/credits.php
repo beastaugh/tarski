2c4277e5093c33f969cd191aabb7c774c45b2d57
Benedict Eastaugh <benedict@eastaugh.net>
2012-01-23 11:35:26 +0000

    Fix current site ref.

--- app/templates/credits.php
+++ app/templates/credits.php
@@ -1,7 +1,7 @@
 <div class="primary content">
     <p><?php _e('Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://tarskitheme.com/">Tarski</a>', 'tarski');
     if (is_multisite()) {
-        $site = get_current_site_name(get_current_site());
+        $site = get_current_site();
         
         printf(__(' | Hosted by %s', 'tarski'),
             '<a href="http://' .
