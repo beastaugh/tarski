ec45562826b2bebbfb8691d29a98a0606fbc4827
Benedict Eastaugh <benedict@eastaugh.net>
2012-06-03 16:10:23 +0100

    Remove 'Hosted by' link from the footer.

--- app/templates/credits.php
+++ app/templates/credits.php
@@ -1,12 +1,3 @@
 <div class="primary content">
-    <p><?php _e('Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://tarskitheme.com/">Tarski</a>', 'tarski');
-    if (is_multisite()) {
-        $site = get_current_site();
-        
-        printf(__(' | Hosted by %s', 'tarski'),
-            '<a href="http://' .
-            $site->domain .
-            $site->path .
-            '">' . $site->site_name . '</a>');
-    } ?></p>
+    <p><?php _e('Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://tarskitheme.com/">Tarski</a>', 'tarski'); ?></p>
 </div>
